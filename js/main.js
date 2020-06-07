9499d67e36cae0c05438fd0c63e287eaf1e431fd
robson rodrigues <robgol_123@hotmail.com>
2020-06-07 13:54:03 -0300

    Comandos Básicos em JS

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/* var nome = "Robson Soares";
+var idade = 22;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " +idade +" anos!");
+//alert(idade + idade2);
+console.log(nome);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil")); */
+
+//var lista = ["maça", "pera", "laranja"];
+//console.log(lista[1]);
+//alert(lista[1]);
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.join(" - "));
+
+/*var fruta = {
+    nome:"maça", cor:"vermelha"
+}
+console.log(fruta.nome);*/
+
+/*var frutas = [
+    {nome:"maça", cor:"vermelha"},
+    {nome:"uva", cor:"roxa"}
+]
+console.log(frutas);*/
+/*
+var idade = prompt("Digite sua idade");
+if(idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+};*/
+
+/*var count = 0;
+while(count <5){
+    console.log(count)
+    count++;
+}*/
+/*
+var count;
+for(count=1; count<=5; count++){
+    console.log(count)
+}
+*/
+var d = new Date();
+//mes começa do 0
+alert(d.getMonth()+1);
