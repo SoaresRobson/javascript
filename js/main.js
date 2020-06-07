21455a22db7b3bbe180a78586dd961e60531e6bb
robson rodrigues <robgol_123@hotmail.com>
2020-06-07 15:24:14 -0300

    Inserindo exemplos de funções JS

--- js/main.js
+++ js/main.js
@@ -45,6 +45,31 @@ for(count=1; count<=5; count++){
     console.log(count)
 }
 */
-var d = new Date();
+/*var d = new Date();
 //mes começa do 0
-alert(d.getMonth()+1);
+alert(d.getMonth()+1);*/
+
+/*function soma(n1, n2){
+    return n1+n2;
+}*/
+
+//alert(soma(10,10));
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+function redirecionar(){
+    window.open("https://www.google.com.br")
+}
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+}
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+function load(){
+    alert("pagina carregada");
+}
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
